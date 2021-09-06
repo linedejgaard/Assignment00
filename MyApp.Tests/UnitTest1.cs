6a3a91869a45023f4a9a5cb8e57bd7182bccd184
ldej <ldej@itu.dk>
2021-09-06 17:45:08 +0200

    Tested and implemented IsLeapYear

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -6,9 +6,69 @@ namespace MyApp.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void is400LeapYear()
         {
+            //Arrange
+            var myApp = new Year();
+            //Act
+            var is400LY = myApp.IsLeapYear(400);
+            //Assert
+            Assert.Equal(true, is400LY);
+        }
 
+        [Fact]
+        public void is1700LeapYear()
+        {
+            //Arrange
+            var myApp = new Year();
+            //Act
+            var is1700LY = myApp.IsLeapYear(1700);
+            //Assert
+            Assert.Equal(false, is1700LY);
+        }
+
+        [Fact]
+        public void is1800LeapYear()
+        {
+            //Arrange
+            var myApp = new Year();
+            //Act
+            var is1800LY = myApp.IsLeapYear(1800);
+            //Assert
+            Assert.Equal(false, is1800LY);
+        }
+
+        [Fact]
+        public void is1900LeapYear()
+        {
+            //Arrange
+            var myApp = new Year();
+            //Act
+            var is1900LY = myApp.IsLeapYear(1900);
+            //Assert
+            Assert.Equal(false, is1900LY);
+        }
+
+        [Fact]
+        public void is1600LeapYear()
+        {
+            //Arrange
+            var myApp = new Year();
+            //Act
+            var isLY = myApp.IsLeapYear(1600);
+            //Assert
+            Assert.Equal(true, isLY);
+        }
+
+        [Fact]
+        public void is2000LeapYear()
+        {
+            //Arrange
+            var myApp = new Year();
+            //Act
+            var isLY = myApp.IsLeapYear(2000);
+            //Assert
+            Assert.Equal(true, isLY);
         }
     }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,11 +2,26 @@
 
 namespace MyApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            
+        }
+    }
+
+    public class Year{
+        public bool IsLeapYear(int year){
+            if (year%400 == 0){
+                return true;
+            } else if(year%100 == 0){
+                return false;
+            } else if(year%4==4){
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 }
