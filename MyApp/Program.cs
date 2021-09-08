6eb4c33375297d509c21cea2eb3c3ffe3cfc65b9
linedejgaard <90201520+linedejgaard@users.noreply.github.com>
2021-09-08 08:22:27 +0200

    Leap year function (#2)
    * add MyApp and MyApp.Tests
    
    * Tested and implemented IsLeapYear
    
    * Created user interface for IsLeapYear

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,11 +2,42 @@
 
 namespace MyApp
 {
-    class Program
+
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the year to check");
+            int input = Int16.Parse(Console.ReadLine());
+            Console.WriteLine(IsLeapYearToString(input));
+
+            
+
+        }
+
+        public static string IsLeapYearToString(int input){
+            
+            
+            var year = new Year();
+            if(year.IsLeapYear(input)){
+                return "yay";
+            } else {
+                return "nay";
+            }
+        }
+    }
+
+    public class Year{
+        public bool IsLeapYear(int year){
+            if (year%400 == 0){
+                return true;
+            } else if(year%100 == 0){
+                return false;
+            } else if(year%4==4){
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 }
