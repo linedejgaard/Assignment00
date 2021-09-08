fedc807da350724111a5cb09cddd10e8c1227e04
ldej <ldej@itu.dk>
2021-09-08 09:07:07 +0200

    Implemented error handling

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace MyApp.Tests
             //Act
             var is400LY = myApp.IsLeapYear(400);
             //Assert
-            Assert.Equal(true, is400LY);
+            Assert.True(is400LY);
         }
 
         [Fact]
@@ -24,7 +24,7 @@ namespace MyApp.Tests
             //Act
             var is1700LY = myApp.IsLeapYear(1700);
             //Assert
-            Assert.Equal(false, is1700LY);
+            Assert.False(is1700LY);
         }
 
         [Fact]
@@ -35,7 +35,7 @@ namespace MyApp.Tests
             //Act
             var is1800LY = myApp.IsLeapYear(1800);
             //Assert
-            Assert.Equal(false, is1800LY);
+            Assert.False(is1800LY);
         }
 
         [Fact]
@@ -46,7 +46,7 @@ namespace MyApp.Tests
             //Act
             var is1900LY = myApp.IsLeapYear(1900);
             //Assert
-            Assert.Equal(false, is1900LY);
+            Assert.False(is1900LY);
         }
 
         [Fact]
@@ -57,7 +57,7 @@ namespace MyApp.Tests
             //Act
             var isLY = myApp.IsLeapYear(1600);
             //Assert
-            Assert.Equal(true, isLY);
+            Assert.True(isLY);
         }
 
         [Fact]
@@ -68,7 +68,7 @@ namespace MyApp.Tests
             //Act
             var isLY = myApp.IsLeapYear(2000);
             //Assert
-            Assert.Equal(true, isLY);
+            Assert.True(isLY);
         }
 
         [Fact]
@@ -80,6 +80,42 @@ namespace MyApp.Tests
             //Act
             //Assert
             Assert.Equal("yay", Program.IsLeapYearToString(1600));
+
+        }
+
+        [Fact]
+        public void is1600LeapYearError()
+        {
+            /*//Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);*/
+            //Act
+            //Assert
+            Assert.Equal("yay", Program.IsLeapYearErrors("1600"));
+
+        }
+
+        [Fact]
+        public void is1500LeapYearError()
+        {
+            /*//Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);*/
+            //Act
+            //Assert
+            Assert.Equal("You have to enter a year after 1582", Program.IsLeapYearErrors("1500"));
+
+        }
+
+        [Fact]
+        public void isLettersLeapYearError()
+        {
+            /*//Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);*/
+            //Act
+            //Assert
+            Assert.Contains("Exception caught.\n\r Please type a year", Program.IsLeapYearErrors("asdf"));
 
         }
     }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -8,11 +8,22 @@ namespace MyApp
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the year to check");
-            int input = Int16.Parse(Console.ReadLine());
-            Console.WriteLine(IsLeapYearToString(input));
-
+            Console.WriteLine(IsLeapYearErrors(Console.ReadLine()));
             
 
+        }
+
+        public static string IsLeapYearErrors(string readline){
+            try{
+                int input = Int16.Parse(readline);
+                if(input>=1582){
+                return IsLeapYearToString(input);
+                } else {
+                return "You have to enter a year after 1582";
+                }
+            } catch (Exception e) {
+                return e.ToString() + "Exception caught.\n\r Please type a year";
+            }
         }
 
         public static string IsLeapYearToString(int input){
